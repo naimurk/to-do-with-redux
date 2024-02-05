f7cd389aaba873f7877f9d63af4eb4c84f079209
unknown <naimurk781@gmail.com>
2024-02-05 20:00:22 +0600

    s

---
+++ src/components/todo/TodoContainer.jsx
@@ -0,0 +1,12 @@
+import TodoCard from "./TodoCard";
+
+
+const TodoContainer = () => {
+    return (
+        <div>
+            <TodoCard></TodoCard>
+        </div>
+    );
+};
+
+export default TodoContainer;
